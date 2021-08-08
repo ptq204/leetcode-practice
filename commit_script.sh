0dd316a66bfe9f217217248996f6b7b7277902c0
ptq204 <ptquyen@apcs.vn>
2021-08-08 17:03:48 +0700

    Add commit command in commit_script

--- commit_script.sh
+++ commit_script.sh
@@ -6,8 +6,10 @@ commit_msg="solved:"
 
 for ufile in $untracked_files
 do
+    git add "$ufile"
     IFS='-' read -a ufile_name <<< "$ufile"
     commit_msg+=" ${ufile_name[0]}"
 done
 
 echo "$commit_msg"
+git commit -m "$commit_msg"
